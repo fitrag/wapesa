a947e907ed77a92aad47883b0ee39968bef6f306
fitrag <jaya88114@gmail.com>
2024-05-03 18:02:38 +0700

    add auth sistem and login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="{{ asset('img/logo1.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+            </div>
+            <h4 class="text-center mb-4">Sistem Informasi Akademik</h4>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Login</h4></div>
+
+              <div class="card-body">
+                <form method="POST" action="#" class="needs-validation" novalidate="">
+                  <div class="form-group">
+                    <label for="username">Username</label>
+                    <input id="username" type="username" class="form-control" name="username" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                      Mohon di isi username anda
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="d-block">
+                    	<label for="password" class="control-label">Password</label>
+                      <div class="float-right">
+                        <a href="auth-forgot-password.html" class="text-small">
+                          Lupa Password?
+                        </a>
+                      </div>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                      Mohon di isi password anda
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                      Login
+                    </button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+            <div class="mt-5 text-muted text-center">
+              Belum punya akun? <a href="auth-register.html">Create One</a>
+            </div>
+            <div class="simple-footer">
+              Copyright &copy; SMKN 1 Way Pengubuan 2024
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('modules/popper.js') }}"></script>
+  <script src="{{ asset('modules/tooltip.js') }}"></script>
+  <script src="{{ asset('modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('modules/moment.min.js') }}"></script>
+  <script src="{{ asset('js/stisla.js') }}"></script>
+  
+  <!-- JS Libraies -->
+
+  <!-- Page Specific JS File -->
+  
+  <!-- Template JS File -->
+  <script src="{{ asset('js/scripts.js') }}"></script>
+  <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
