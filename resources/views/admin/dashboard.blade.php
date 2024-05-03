b9876028c4df530e988bc74df589bc223a7b1bdc
fitrag <jaya88114@gmail.com>
2024-05-03 19:18:16 +0700

    update sidebar menu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 
     <section class="section">
         <div class="section-header">
-        <h1>Blank Page</h1>
+        <h1>Dashboard</h1>
         </div>
 
         <div class="section-body">
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,17 +7,22 @@
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="active"><a class="nav-link" href="blank.html"><i class="fa fa-tachometer-alt"></i> <span>Dashboard</span></a></li>
+        <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Dashboard</span></a></li>
         <li class="menu-header">Data Master</li>
+        <li><a class="nav-link"><i class="fas fa-users"></i> <span>Data User</span></a></li>
+        <li><a class="nav-link"><i class="fas fa-user-graduate"></i> <span>Data Siswa</span></a></li>
+        <li><a class="nav-link"><i class="fas fa-user-tie"></i> <span>Data Guru</span></a></li>
         <li class="dropdown">
             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
             <ul class="dropdown-menu">
-            <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-            <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-            <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
             </ul>
         </li>
-        <li class=""><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+        <li><a class="nav-link"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+        <li class="menu-header">Lainnya</li>
+        <li><a class="nav-link"><i class="fas fa-cog"></i> <span>Pengaturan</span></a></li>
     </ul>
 
     <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
