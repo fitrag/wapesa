007598d03fff8c55282abb7ea8ed716558e3d2f2
fitrag <jaya88114@gmail.com>
2024-05-04 11:28:55 +0700

    update fitur login dan register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\{Hash, Auth};
 
 class AuthController extends Controller
 {
     public function login(){
         return view('auth.login');
+    }
+    public function auth(Request $req){
+        $credentials = $req->validate([
+            'username'      => 'required',
+            'password'      => 'required',
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $req->session()->regenerate();
+            return redirect()->route('dashboard');
+        }
+
+        return back();
+    }
+    public function register(){
+        return view('auth.register');
+    }
+    public function store(Request $req){
+        $req->validate([
+            'username'  => 'required',
+            'password'  => 'required'
+        ]);
+
+        $user = User::create([
+            'name'      => $req->username,
+            'username'  => $req->username,
+            'password'  => Hash::make($req->password),
+        ]);
+
+        if($user){
+            return redirect()->route('login');
+        }else{
+            return redirect()->back();
+        }
     }
 }
