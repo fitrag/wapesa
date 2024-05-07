b984a7ba45fde6537261453f288fd0ceb80ba463
yantojho <yantojho@gmail.com>
2024-05-07 13:58:57 +0700

    up

--- resources/views/admin/kelas/index_kls.blade.php
+++ resources/views/admin/kelas/index_kls.blade.php
@@ -13,7 +13,69 @@
 @section('content')
 
 <section class="section">
+    <div class="section-header">
+    <h1>Data User</h1>
+    <div class="section-header-breadcrumb">
+        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+        <div class="breadcrumb-item"><a href="#">Data Master</a></div>
+        <div class="breadcrumb-item">Data Kelas</div>
+    </div>
+    </div>
+
+    <div class="section-body">
+    <h2 class="section-title">Data Kelas</h2>
     
+
+    <div class="row">
+        <div class="col-12">
+            @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if(session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+        <div class="card">
+            <div class="">
+            <button class="btn btn-primary float-right mb-4" data-target="#exampleModal" data-toggle="modal">Tambah data kelas</button>
+            </div>
+            <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped" id="table-1">
+                <thead>                                 
+                    <tr>
+                        <th class="text-center">
+                            #
+                        </th>
+                        <th>Nama Kelas</th>
+                        <th>Alias</th>
+                    </tr>
+                </thead>
+                <tbody>                                 
+                    
+                </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
 </section>
+
+        <div class="modal fade" tabindex="-1" role="dialog" id="exampleModal">
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Tambah data kelas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                
+              </div>
+            </div>
+          </div>
+        </div>
 
 @endsection
