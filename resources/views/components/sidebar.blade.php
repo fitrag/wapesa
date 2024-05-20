7149bb419a6c545589afa5c6662398ef27ba3418
fitrag <jaya88114@gmail.com>
2024-05-20 10:34:36 +0700

    memperbarui tampilan sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -30,14 +30,6 @@
         <li class="menu-header">Lainnya</li>
         <li class="{{ request()->is('admin/pengaturan*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('pengaturan') }}"><i class="fas fa-cog"></i> <span>Pengaturan</span></a></li>
         <li class="dropdown">
-            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-            </ul>
-        </li>
-        <li class="dropdown">
             <a href="#" class="nav-link has-dropdown bg-warning text-white" data-toggle="dropdown"><i class="fas fa-flask"></i> <span>Experiment</span></a>
             <ul class="dropdown-menu">
                 <li><a class="nav-link" href="{{ route('sinkron-absensi') }}">Sinkron Absensi</a></li>
